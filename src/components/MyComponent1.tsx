75cc7e8158c96703dcc00bd9c79e99d249a5461e
joel.pettersson90@gmail.com <joel.pettersson90@gmail.com>
2022-03-11 16:08:46 +0100

    done, i think....

---
+++ src/components/MyComponent1.tsx
@@ -0,0 +1,15 @@
+export function MyComponent1() {
+    return (
+    <div>
+        <h1>Den första texten</h1>
+        <br></br>
+        <h2>Den andra texten</h2>
+        <br></br>
+        <input></input>
+        <br></br>
+        <br></br>
+        <button>Lägg ihop</button>
+    </div>
+    )
+    
+}
